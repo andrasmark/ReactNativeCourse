5485a9b0672cb087fe175b372ff89918923dc53b
andrasmark <mrkandras10@gmail.com>
2025-09-17 15:36:48 +0300

    Finished the basics of the guessing game.

---
+++ guessing_game/components/ui/PrimaryButton.jsx
@@ -0,0 +1,43 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
+function PrimaryButton({ children, onPressed }) {
+  
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={onPressed}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: Colors.primary500,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    textAlign: "center",
+    fontFamily: "open-sans",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
