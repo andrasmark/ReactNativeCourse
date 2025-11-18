0b904b85021c545ce8262b4c7dd6b9e46f37c752
andrasmark <mrkandras10@gmail.com>
2025-11-18 09:49:11 +0200

    Users can locate themselves

--- favourite_places/components/Places/ImagePicker.jsx
+++ favourite_places/components/Places/ImagePicker.jsx
@@ -1,4 +1,7 @@
-import { View, Button, Alert } from "react-native";
+import { View, Alert, Image, StyleSheet, Text } from "react-native";
+import { useState } from "react";
+import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
 import {
   launchCameraAsync,
   useCameraPermissions,
@@ -6,6 +9,8 @@ import {
 } from "expo-image-picker";
 
 function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState();
+
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
 
@@ -37,14 +42,38 @@ function ImagePicker() {
       quality: 0.5,
     });
     console.log(image);
+    setPickedImage(image.assets[0].uri);
+  }
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
   }
 
   return (
     <View>
-      <View></View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon="camera"onPress={takeImageHandler}>Take Image</OutlinedButton>
     </View>
   );
 }
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
---
+++ favourite_places/components/UI/OutlinedButton.jsx
@@ -0,0 +1,42 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import { Colors } from "../../constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+
+function OutlinedButton({ onPress, icon, children }) {
+  return (
+    <Pressable style={({pressed}) => [styles.button, pressed && styles.pressed]} onPress={onPress}>
+      <Ionicons
+        style={styles.icon}
+        name={icon}
+        size={18}
+        color={Colors.primary500}
+      />
+      <Text style={styles.text}>{children}</Text>
+    </Pressable>
+  );
+}
+
+export default OutlinedButton;
+
+const styles = StyleSheet.create({
+  button: {
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    margin: 4,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    //borderRadius: 4,
+    borderColor: Colors.primary500,
+    borderWidth: 1,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  icon: {
+    marginRight: 6,
+  },
+  text: {
+    color: Colors.primary500,
+  },
+});
